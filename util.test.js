972e6018a7d269b7b64098f64874e46b193fe386
Akos Somodi <akossomodi@yahoo.com>
2020-04-23 16:28:26 +0200

    create e2e test part 2

--- util.test.js
+++ util.test.js
@@ -14,8 +14,8 @@ test('should generate a valid text output', () => {
 test('should click around', async () => {
   const browser = puppeteer.launch({
     headless: false,
-    slowMo: 80,
-    args: ['--window-size=1920,1080'],
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080'],
   });
   const page = await (await browser).newPage();
   await page.goto('https://akossomodi.com/');
@@ -24,4 +24,6 @@ test('should click around', async () => {
   await page.click('input#age');
   await page.type('input#age', '28');
   await page.click('#btnAddUser');
-});
+  const finalText = await page.$eval('.user-item', (el) => el.textContent);
+  expect(finalText).toBe('Anna (28 years old)');
+}, 10000);
