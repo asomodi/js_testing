86f37fe17cbe226cab93e3c15502bf232304b22b
Akos Somodi <akossomodi@yahoo.com>
2020-04-23 16:02:33 +0200

    create integration test

--- util.test.js
+++ util.test.js
@@ -5,7 +5,7 @@ test('should output name and age', () => {
   expect(text).toBe('Max (29 years old)');
 });
 
-test('should output data-less text', () => {
-  const text = generateText('', null);
-  expect(text).toBe(' (null years old)');
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Max', 29);
+  expect(text).toBe('Max (29 years old)');
 });
